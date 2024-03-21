409b58349ac3a9b294bfa11ae33cface0fe6c132
Danil Sidoruk <danilsidoruknv@yandex.ru>
2024-03-21 22:26:55 +0300

    Implement basic functions

--- README.md
+++ README.md
@@ -5,7 +5,9 @@
 ## Использование
 
 1. Установите следующие environment variables:
+* `T2LOGSEQ_BOT_TOKEN`
 * `T2LOGSEQ_JOURNALS_PATH` — путь к директории с журналами;
 * `T2LOGSEQ_FILE_FORMAT` — формат названий журнальных файлов;
+* `T2LOGSEQ_TIME_FORMAT` — формат временных меток;
 * `T2LOGSEQ_ADMIN_USERNAME` — админский username, сообщения с иных username будут игнорироваться;
 2. Запустите ретранслятор: `poetry run python3 -m telegram2logseq`
--- telegram2logseq/__main__.py
+++ telegram2logseq/__main__.py
@@ -1,19 +1,42 @@
 import logging
+import os
+from datetime import datetime
 
+from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
 
+load_dotenv()
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+T2LOGSEQ_BOT_TOKEN = os.getenv("T2LOGSEQ_BOT_TOKEN")
+T2LOGSEQ_JOURNALS_PATH = os.getenv("T2LOGSEQ_JOURNALS_PATH")
+T2LOGSEQ_FILE_FORMAT = os.getenv("T2LOGSEQ_FILE_FORMAT")
+T2LOGSEQ_TIME_FORMAT = os.getenv("T2LOGSEQ_TIME_FORMAT")
+T2LOGSEQ_ADMIN_USERNAME = os.getenv("T2LOGSEQ_ADMIN_USERNAME")
+
+
+async def retranslate(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message.from_user.username != T2LOGSEQ_ADMIN_USERNAME:
+        return
+
+    current_file = f"{datetime.now().strftime(T2LOGSEQ_FILE_FORMAT)}.md"
+    with open(os.path.join(T2LOGSEQ_JOURNALS_PATH, current_file), "a") as f:
+        print("\n-", datetime.now().strftime(T2LOGSEQ_TIME_FORMAT), file=f)
+        print("\t-", update.message.text, file=f)
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Got it")
 
 
 if __name__ == "__main__":
-    application = ApplicationBuilder().token("TOKEN").build()
+    if not os.path.isdir(T2LOGSEQ_JOURNALS_PATH):
+        print("Invalid journals path")
+        os.exit(1)
 
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
-    application.add_handler(echo_handler)
+    application = ApplicationBuilder().token(T2LOGSEQ_BOT_TOKEN).connect_timeout(1000).build()
+
+    retranslate_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), retranslate)
+    application.add_handler(retranslate_handler)
 
     application.run_polling()
